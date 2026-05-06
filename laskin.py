5ed73a3e6411130fb4d4ca3ccff9d98819784f3e
Heikki Loukojärvi <heikki.loukojarvi@keuda.fi>
2026-05-06 13:17:33 +0300

    Lisätty vähennyslasku-funktio

--- laskin.py
+++ laskin.py
@@ -1,8 +1,10 @@
 # Laskin v1.0
-
 def yhteenlasku(a, b):
     return a + b
  
+def vähennyslasku(a, b):
+    return a - b
+ 
 print("Laskin v1.0")
 print("5 + 3 =", yhteenlasku(5, 3))
-
+print("10 - 4 =", vähennyslasku(10, 4))
